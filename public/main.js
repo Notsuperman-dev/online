49ee8259cc1b0c93569a23c6792c1242216ce6c1
Notsuperman-dev <supermanonipad@gmail.com>
2024-06-07 00:00:32 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,4 +1,10 @@
+// public/main.js
 const socket = io();
+const countElement = document.getElementById('count');
+
+socket.on('userCount', (count) => {
+  countElement.textContent = count;
+});
 
 const messageInput = document.getElementById('message-input');
 const sendButton = document.getElementById('send-button');
